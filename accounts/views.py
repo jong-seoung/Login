9c71886cb318a806cdd24367a079e2e6b02e4e41
백종성 <jongseoung@baegjongseong-ui-MacBookAir.local>
2023-06-28 22:59:48 +0900

    feat: 일반 회원가입 기능

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .forms import SignUpForm
 
-# Create your views here.
+
+class SignUpAPIView(APIView):
+    def post(self, request):
+        form = SignUpForm(request.data)
+        if form.is_valid():
+            form.save()
+            return Response({'message': 'User created successfully.'}, status=status.HTTP_201_CREATED)
+        return Response(form.errors, status=status.HTTP_400_BAD_REQUEST)
